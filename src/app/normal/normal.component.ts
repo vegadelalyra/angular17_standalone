9c72b453c71132025164b9483f5617d1c33773c0
Vegadelalyra <lyradeperseo@gmail.com>
2024-08-11 18:28:16 -0500

    feat: ng17 standalone standard

---
+++ src/app/normal/normal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-normal',
+  standalone: true,
+  imports: [],
+  templateUrl: './normal.component.html',
+  styleUrl: './normal.component.sass'
+})
+export class NormalComponent {
+
+}
