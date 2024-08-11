645d09c82ddfa1ce5fc3d09c28d7992e8bfe05c6
Vegadelalyra <lyradeperseo@gmail.com>
2024-08-11 18:49:38 -0500

    ng17 standalone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    // loadComponent: () =>
+    //   import('./admin/admin.component').then((m) => m.AdminComponent),
+  },
+];
